81dbf1244cc78fecee4f4414d28f687878e233ec
raj <razbc12@gmail.com>
2025-01-15 14:07:49 +0545

    Add light up effect to the row/column that wins

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ tileContainer.addEventListener('click', (event) => {
     let target = event.target;
     switch(target.getAttribute('class')){
         case 'tile':
-            gameBoard.placeChoice(target.getAttribute('data-index'), turn);
             target.textContent = turn;
+            gameBoard.placeChoice(target.getAttribute('data-index'), turn);
             turnToggle();
             target.disabled = true;
             break;
@@ -23,6 +23,7 @@ function turnToggle(){
 const gameBoard = (function(){
     const tiles = Object.seal(['', '', '', '', '', '', '', '', '']);
     let tilesOnScreen = document.querySelectorAll('.tile');
+    let winTrio = [];
 
     function placeChoice(index, choice){
         tiles[index] = choice;
@@ -33,24 +34,26 @@ const gameBoard = (function(){
         let victor = '';
 
         //row-check//
-        if(tiles[0] === placeholder && tiles[1] === placeholder && tiles[2] === placeholder) victor = placeholder;
-        if(tiles[3] === placeholder && tiles[4] === placeholder && tiles[5] === placeholder) victor = placeholder;
-        if(tiles[6] === placeholder && tiles[7] === placeholder && tiles[8] === placeholder) victor = placeholder;
+        if(tiles[0] === placeholder && tiles[1] === placeholder && tiles[2] === placeholder) {victor = placeholder; winTrio = [0, 1, 2];}
+        if(tiles[3] === placeholder && tiles[4] === placeholder && tiles[5] === placeholder) {victor = placeholder; winTrio = [3, 4, 5];}
+        if(tiles[6] === placeholder && tiles[7] === placeholder && tiles[8] === placeholder) {victor = placeholder; winTrio = [6, 7, 8];}
 
         //column-check//
-        if(tiles[0] === placeholder && tiles[3] === placeholder && tiles[6] === placeholder) victor = placeholder;
-        if(tiles[1] === placeholder && tiles[4] === placeholder && tiles[7] === placeholder) victor = placeholder;
-        if(tiles[2] === placeholder && tiles[5] === placeholder && tiles[8] === placeholder) victor = placeholder;
+        if(tiles[0] === placeholder && tiles[3] === placeholder && tiles[6] === placeholder) {victor = placeholder; winTrio = [0, 3, 6];}
+        if(tiles[1] === placeholder && tiles[4] === placeholder && tiles[7] === placeholder) {victor = placeholder; winTrio = [1, 4, 7];}
+        if(tiles[2] === placeholder && tiles[5] === placeholder && tiles[8] === placeholder) {victor = placeholder; winTrio = [2, 5, 8];}
 
         //diagonal-check//
-        if(tiles[0] === placeholder && tiles[4] === placeholder && tiles[8] === placeholder) victor = placeholder;
-        if(tiles[2] === placeholder && tiles[4] === placeholder && tiles[6] === placeholder) victor = placeholder;
+        if(tiles[0] === placeholder && tiles[4] === placeholder && tiles[8] === placeholder) {victor = placeholder; winTrio = [0, 4, 8];}
+        if(tiles[2] === placeholder && tiles[4] === placeholder && tiles[6] === placeholder) {victor = placeholder; winTrio = [2, 4, 6];}
 
         if(victor === 'x'){
             resultDiv.textContent = `Player 1 Wins!`;
+            lightUpWin(winTrio);
             disableTilesOnWin();
         }else if(victor === 'o'){
             resultDiv.textContent = `Player 2 Wins!`;
+            lightUpWin(winTrio);
             disableTilesOnWin();
         }else if((tiles[0] && tiles[1] && tiles[2] && tiles[3] && tiles[4] && tiles[5] && tiles[6] && tiles[7] && tiles[8]) && !victor){
             resultDiv.textContent = "Its's a Draw!";
@@ -67,12 +70,24 @@ const gameBoard = (function(){
             turn = 'x';
         });
         resultDiv.textContent = '';
+        unlight(winTrio);
     }
 
     function disableTilesOnWin(){
         tilesOnScreen.forEach(element => {
             element.disabled = true;
         });
+    }
+
+    function lightUpWin(array){
+        document.querySelector(`[data-index = '${array[0]}']`).style.color = 'green';
+        document.querySelector(`[data-index = '${array[1]}']`).style.color = 'green';
+        document.querySelector(`[data-index = '${array[2]}']`).style.color = 'green';
+    }
+    function unlight(array){
+        document.querySelector(`[data-index = '${array[0]}']`).style.color = '#A294F9';
+        document.querySelector(`[data-index = '${array[1]}']`).style.color = '#A294F9';
+        document.querySelector(`[data-index = '${array[2]}']`).style.color = '#A294F9';
     }
 
     return {
